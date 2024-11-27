64ba2e1319a3a621213befa4ff53564553557e26
pabloacinas <UO287623@uniovi.es>
2024-11-27 15:57:59 +0100

    Red Phase - US1

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -9,7 +9,17 @@ from src.intelligentoffice import IntelligentOffice, IntelligentOfficeError
 
 class TestIntelligentOffice(unittest.TestCase):
 
+
+    """
+    The office is divided into four quadrants. On the ceiling of each quadrant, there is an infrared distance sensor that detects the presence of a worker in that quadrant.
+
+The infrared distance sensors are connected, respectively, to pins 11, 12, 13, and 15 (BOARD mode). The communication with the sensors happens via the GPIO.input(channel) function (where channel is the pin). Specifically, this function returns True if a worker is detected by the sensor, False otherwise.
+
+Note that the sensors have already been set up in the constructor of the IntelligentOffice class.
+    """
+
     @patch.object(GPIO, "input")
-    def test_something(self, mock_object: Mock):
-        # This is an example of test where I want to mock the GPIO.input() function
-        pass
+    def test_check_cuadrant_is_occupied(self, mock_infrared: Mock):
+        mock_infrared.return_value = True
+        io = IntelligentOffice()
+        self.assertTrue(io.check_quadrant_occupancy(11))
