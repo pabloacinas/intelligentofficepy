1117196bd2b7e62937936b50c63b953388d2e325
pabloacinas <UO287623@uniovi.es>
2024-11-27 16:16:52 +0100

    Red Phase - US2

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -9,15 +9,6 @@ from src.intelligentoffice import IntelligentOffice, IntelligentOfficeError
 
 class TestIntelligentOffice(unittest.TestCase):
 
-
-    """
-    The office is divided into four quadrants. On the ceiling of each quadrant, there is an infrared distance sensor that detects the presence of a worker in that quadrant.
-
-The infrared distance sensors are connected, respectively, to pins 11, 12, 13, and 15 (BOARD mode). The communication with the sensors happens via the GPIO.input(channel) function (where channel is the pin). Specifically, this function returns True if a worker is detected by the sensor, False otherwise.
-
-Note that the sensors have already been set up in the constructor of the IntelligentOffice class.
-    """
-
     @patch.object(GPIO, "input")
     def test_check_cuadrant_1_is_occupied(self, mock_infrared: Mock):
         mock_infrared.return_value = True
@@ -41,3 +32,10 @@ Note that the sensors have already been set up in the constructor of the Intelli
         mock_infrared.return_value = True
         io = IntelligentOffice()
         self.assertTrue(io.check_quadrant_occupancy(15))
+
+    @patch.object(SDL_DS3231, "read_datetime")
+    def test_open_blinds_at_8_am(self, mock_read_datetime: Mock):
+        mock_read_datetime.return_value = datetime(2024, 11, 25, 8, 0) #Monday at 8:00
+        io = IntelligentOffice()
+        io.manage_blinds_based_on_time()
+        self.assertTrue(io.blinds_open)
